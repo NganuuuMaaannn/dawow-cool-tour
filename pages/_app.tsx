b9f080ac3b616b3cf07439e0902d05e9754ea2eb
NganuuuMaaannn <seanmichael.doinog@hcdc.edu.ph>
2025-08-10 10:30:01 -0400

    Updating main page design

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { AnimatePresence, motion } from "framer-motion";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={router.route}
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }} 
+        exit={{ opacity: 0, y: -50 }}  
+        transition={{ duration: 0.6 }}
+      >
+        <Component {...pageProps} />
+      </motion.div>
+    </AnimatePresence>
+  );
 }
