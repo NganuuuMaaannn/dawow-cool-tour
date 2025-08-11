a13d0e2fa1d5c2373e66c7b1e0b9701ae2573b2e
NganuuuMaaannn <seanmichael.doinog@hcdc.edu.ph>
2025-08-11 13:46:13 -0400

    Fix errors in updating

--- pages/mainPage.tsx
+++ pages/mainPage.tsx
@@ -6,7 +6,14 @@ import Footer from "@/components/Footer";
 
 export default function mainPage() {
   return (
-    <div className="flex flex-col min-h-screen bg-bgLogin">
+    <div className="flex flex-col min-h-screen bg-bgLogin relative">
+      
+      {/* Background Section */}
+      <div className="absolute top-0 left-0 w-full h-full z-0">
+        <div className="h-[35vh] lg:h-[50vh] bg-bgTour"></div>
+      </div>
+
+      {/* Content */}
       <Header />
       <main className="flex-grow">
         <MainContent />
