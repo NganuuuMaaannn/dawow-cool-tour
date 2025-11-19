bc4606df22a0bb542b8104e38c654a84294d7f8b
NganuuuMaaannn <seanmichael.doinog@hcdc.edu.ph>
2025-11-19 13:39:14 +0800

    Fix footer logo placement

--- components/Footer-Credits.tsx
+++ components/Footer-Credits.tsx
@@ -6,13 +6,13 @@ import Link from "next/link";
 
 import dvo from "../image/dvo.png";
 import logo1 from "@/image/Logo/cca.jpg";
-import logo2 from "@/image/Logo/chatme.jpg";
+import logo2 from "@/image/Logo/chatme.png";
 import logo3 from "@/image/Logo/dc logo.jpg";
 import logo4 from "@/image/Logo/DOT.png";
 import logo5 from "@/image/Logo/hcdc_logo.png";
 import logo6 from "@/image/Logo/ids.jpg";
 import logo8 from "@/image/Logo/love-philippines-logo-png_seeklogo-619869.png";
-import logo9 from "@/image/Logo/tourism logo.jpg";
+import logo9 from "@/image/Logo/tourism logo.png";
 
 
 export default function Footer() {
@@ -38,25 +38,25 @@ export default function Footer() {
           © {new Date().getFullYear()} Love, Davao! All rights reserved.
         </div>
 
-        <div className="flex flex-wrap justify-center items-center gap-6">
+        <div className="w-full flex flex-wrap justify-center items-center gap-x-8 gap-y-4">
 
-          <Image src={logo8} alt="Logo 8" width={80} height={40} className="object-contain hover:opacity-80 hover:scale-105 transition" />
+          <Image src={logo4} alt="Logo 8" width={60} height={40} className="object-contain hover:opacity-80 hover:scale-105 transition" />
 
-          <Image src={logo9} alt="Logo 9" width={60} height={40} className="object-contain hover:opacity-80 hover:scale-105 transition" />
+          <Image src={logo8} alt="Logo 9" width={80} height={40} className="object-contain hover:opacity-80 hover:scale-105 transition" />
 
-          <Image src={logo1} alt="Logo 1" width={60} height={40} className="object-contain hover:opacity-80 hover:scale-105 transition" />
+          <Image src={logo3} alt="Logo 1" width={60} height={40} className="object-contain hover:opacity-80 hover:scale-105 transition" />
 
-          <Image src={logo2} alt="Logo 2" width={60} height={40} className="object-contain hover:opacity-80 hover:scale-105 transition" />
+          <Image src={logo9} alt="Logo 2" width={100} height={40} className="object-contain hover:opacity-80 hover:scale-105 transition" />
 
-          <Image src={logo6} alt="Logo 6" width={60} height={40} className="object-contain hover:opacity-80 hover:scale-105 transition" />
+          <Image src={dvo} alt="Logo 6" width={120} height={40} className="object-contain hover:opacity-80 hover:scale-105 transition" />
 
-          <Image src={logo4} alt="Logo 4" width={60} height={40} className="object-contain hover:opacity-80 hover:scale-105 transition" />
+          <Image src={logo5} alt="Logo 4" width={60} height={40} className="object-contain hover:opacity-80 hover:scale-105 transition" />
 
-          <Image src={logo5} alt="Logo 5" width={60} height={40} className="object-contain hover:opacity-80 hover:scale-105 transition" />
+          <Image src={logo1} alt="Logo 5" width={60} height={40} className="object-contain hover:opacity-80 hover:scale-105 transition" />
 
-          <Image src={logo3} alt="Logo 3" width={60} height={40} className="object-contain hover:opacity-80 hover:scale-105 transition" />
+          <Image src={logo6} alt="Logo 3" width={60} height={40} className="object-contain hover:opacity-80 hover:scale-105 transition" />
 
-          <Image src={dvo} alt="Logo 7" width={120} height={40} className="object-contain hover:opacity-80 hover:scale-105 transition" />
+          <Image src={logo2} alt="Logo 7" width={60} height={40} className="object-contain hover:opacity-80 hover:scale-105 transition" />
 
         </div>
       </div>
